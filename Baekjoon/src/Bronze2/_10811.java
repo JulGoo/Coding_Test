e4c2df944bdd968608a4c3106008ab54fd04c3eb
JulGoo <yrruda94@gmail.com>
2023-11-02 23:43:04 +0900

    231102

---
+++ Baekjoon/src/Bronze2/_10811.java
@@ -0,0 +1,44 @@
+package Bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10811 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
+        int temp = 0;
+
+        for (int j = 1; j <= n; j++) {
+            arr[j] = j;
+            System.out.println(j + ":" + arr[j]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st1.nextToken());
+            int b = Integer.parseInt(st1.nextToken());
+
+            while (a != b) {
+                temp = arr[a];
+                System.out.println("arr[a]:"+arr[a]+"/a:"+a);
+                System.out.println("arr[b]:"+arr[b]+"/b:"+b);
+                System.out.println("temp:"+temp);
+                arr[a] = arr[b];
+                arr[b] = temp;
+
+                a++;
+                b--;
+            }
+        }
+
+        for (int j = 1; j <= n; j++) {
+            System.out.print(arr[j]);
+        }
+    }
+}
