19ea483415c27564509b6302be6ea95958762c59
JulGoo <yrruda94@gmail.com>
2024-02-04 23:33:31 +0900

    240204

---
+++ Baekjoon/src/Bronze2/_25305.java
@@ -0,0 +1,27 @@
+package Bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st1.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n - k]);
+    }
+}
