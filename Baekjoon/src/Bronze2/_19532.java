49a6633e88c911690dd3e0a61e04c5e10c86bec4
JulGoo <yrruda94@gmail.com>
2024-02-13 19:58:09 +0900

    240213

---
+++ Baekjoon/src/Bronze2/_19532.java
@@ -0,0 +1,36 @@
+package Bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        int g = a + d;
+        int h = b + e;
+        int l = c + f;
+
+        for (int i = -999; i <= 999; i++) {
+            for (int j = -999; j <= 999; j++) {
+                if ((g * i) == (l - h * j)) {
+                    if ((a * i) + (b * j) == c) {
+                        if ((d * i) + (e * j) == f) {
+                            System.out.println(i + " " + j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
